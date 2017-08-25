39b39614b95c260c4619b7bb06249f77bcd4299c
yunfeiroy666 <yunfeiroy@gmail.com>
2017-08-26 00:01:05 +0800

    add something into generate_feedvector

--- s3_discover_group/generate_feedvector.py
+++ s3_discover_group/generate_feedvector.py
@@ -8,7 +8,7 @@ import re
 def get_word_counts(url):
     # 解析订阅源
     d = feedparser.parse(url)
-    wc = {}
+    word_count = {}
 
     # 循环遍历所有的文章条目
     for e in d.entries:
@@ -20,11 +20,12 @@ def get_word_counts(url):
         # 提取一个单词列表
         words = get_words(e.title + ' ' + summary)
         for word in words:
-            wc.setdefault(word, 0)
-            wc[word] += 1
-    return d.feed.title, wc
+            word_count.setdefault(word, 0)
+            word_count[word] += 1
+    return d.feed.title, word_count
 
 
+# 提取单词列表
 def get_words(html):
     # 去除所有HTML标记
     txt = re.compile(r'<[^>]+>').sub('', html)
@@ -34,3 +35,35 @@ def get_words(html):
 
     # 转化成小写形式
     return [word.lower() for word in words if word != '']
+
+
+apcount = {}
+wordcounts = {}
+feedlist = [line for line in file('feedlist.txt')]
+for feed_url in feedlist:
+    title, word_count = get_word_counts(feed_url)
+    wordcounts[title] = word_count
+    for word, count in word_count.items():
+        apcount.setdefault(word, 0)
+        if count > 1:
+            apcount[word] += 1
+
+word_list = []
+for w, bc in apcount.items():
+    frac = float(bc) / len(feedlist)
+    if frac > 0.1 and frac < 0.5:
+        word_list.append(w)
+
+out = file('blog_data.txt', 'w')
+out.write('Blog')
+for word in word_list:
+    out.write('\t%s' % word)
+out.write('\n')
+for blog, wc in wordcounts.items():
+    out.write(blog)
+    for word in word_list:
+        if word in wc:
+            out.write('\t%d' % wc[word])
+        else:
+            out.write('\t0')
+    out.write('\n')
