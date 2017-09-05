f9f6c17b581a204bd54a1d23a43e135e701e88b9
yunfeiroy666 <yunfeiroy@gmail.com>
2017-09-05 22:55:09 +0800

    add drawnode

--- s3_discover_group/clusters.py
+++ s3_discover_group/clusters.py
@@ -140,6 +140,32 @@ def get_depth(clust):
     return max(get_depth(clust.left), get_depth(clust.right)) + clust.distance
 
 
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = get_height(clust.left) * 20
+        h2 = get_height(clust.right) * 20
+        top = y - (h1 + h2) / 2
+        bottom = y + (h1 + h2) / 2
+
+        # 线的长度
+        l1 = clust.distance * scaling
+        # 聚类到其子节点的垂直线
+        draw.line((x, top + h1 / 2, x, bottom - h1 / 2), fill=(255, 0, 0))
+
+        # 连接左侧节点的水平线
+        draw.line((x, top + h1 / 2, x + l1, top + h1 / 2), fill=(255, 0, 0))
+
+        # 连接右侧节点的水平线
+        draw.line((x, bottom - h2 / 2, x + l1, bottom - h2 / 2), fill=(255, 0, 0))
+
+        # 调用函数绘制左右节点
+        drawnode(draw, clust.left, x + l1, top + h1 / 2, scaling, labels)
+        drawnode(draw, clust.right, x + l1, bottom - h2 / 2, scaling, labels)
+    else:
+        # 如果这是叶节点，则绘制节点的标签
+        draw.text((x + 5, y - 7), labels[clust.id], (0, 0, 0))
+
+
 # 生成图片
 def drawdendgram(clust, labels, jpeg='clusters.jpg'):
     # 高度和宽度
@@ -148,7 +174,9 @@ def drawdendgram(clust, labels, jpeg='clusters.jpg'):
     depth = get_depth(clust)
 
     # 由于宽度是固定的，因此我们需要对距离值做相应的调整
+    scaling = float(w - 150) / depth
 
+    # 新建一个白色背景的图片
     img = Image.new('RGB', (w, h), (255, 255, 255))
     draw = ImageDraw.Draw(img)
 
@@ -156,6 +184,7 @@ def drawdendgram(clust, labels, jpeg='clusters.jpg'):
 
     # 画第一个节点
     drawnode(draw, clust, 10, (h / 2), scaling, labels)
+    img.save(jpeg, 'JPEG')
 
 
 if __name__ == "__main__":
